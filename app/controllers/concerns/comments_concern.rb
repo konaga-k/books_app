55cdbe00ec21afec751c77c3e233c89457abc70c
konaga-k <priests2020@gmail.com>
2021-05-29 16:26:26 +0900

    Correct by Rubocop

--- app/controllers/concerns/comments_concern.rb
+++ app/controllers/concerns/comments_concern.rb
@@ -1,20 +1,21 @@
+# frozen_string_literal: true
+
 module CommentsConcern
   private
+    def set_commentable
+      resource, id = request.path.split("/")[1, 2]
+      @commentable = resource.singularize.classify.constantize.find(id)
+    end
 
-  def set_commentable
-    resource, id = request.path.split('/')[1,2]
-    @commentable = resource.singularize.classify.constantize.find(id)
-  end
+    def set_new_comment
+      @comment = @commentable.comments.build(comment_params)
+    end
 
-  def set_new_comment
-    @comment = @commentable.comments.build(comment_params)
-  end
+    def set_comment
+      @comment = @commentable.comments.find_by(id: params[:id], user: current_user)
+    end
 
-  def set_comment
-    @comment = @commentable.comments.find_by(id: params[:id], user: current_user)
-  end
-
-  def comment_params
-    params.require(:comment).permit(:body)
-  end
+    def comment_params
+      params.require(:comment).permit(:body)
+    end
 end
